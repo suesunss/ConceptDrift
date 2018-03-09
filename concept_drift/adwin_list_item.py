b76e96d737ce51f2a098c2591f4000791e659257
Alexey Egorov <alexey.egorov@tu-dortmund.de>
2018-03-09 18:01:52 +0100

    replace redundant calculation with a variable

--- concept_drift/adwin_list_item.py
+++ concept_drift/adwin_list_item.py
@@ -4,6 +4,7 @@ import numpy as np
 class AdwinListItem:
     def __init__(self, max_buckets=5, next=None, previous=None):
         self.max_buckets = max_buckets
+        # current number of buckets in this list item
         self.bucket_size_row = 0
 
         self.next = next
@@ -29,10 +30,11 @@ class AdwinListItem:
     def compress_buckets_row(self, number_deleted):
         """Remove the bucket
         """
-        self.bucket_total[:self.max_buckets - number_deleted + 1] = self.bucket_total[number_deleted:]
-        self.bucket_total[self.max_buckets - number_deleted + 1:] = np.zeros(number_deleted)
+        delete_index = self.max_buckets - number_deleted + 1
+        self.bucket_total[:delete_index] = self.bucket_total[number_deleted:]
+        self.bucket_total[delete_index:] = np.zeros(number_deleted)
 
-        self.bucket_variance[:self.max_buckets - number_deleted + 1] = self.bucket_variance[number_deleted:]
-        self.bucket_variance[self.max_buckets - number_deleted + 1:] = np.zeros(number_deleted)
+        self.bucket_variance[:delete_index] = self.bucket_variance[number_deleted:]
+        self.bucket_variance[delete_index:] = np.zeros(number_deleted)
 
         self.bucket_size_row -= number_deleted
